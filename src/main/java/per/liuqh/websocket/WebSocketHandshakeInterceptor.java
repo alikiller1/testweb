705da95410366f5f6dda5cf0dc60c3c6dc5b0ea0
alikiller1 <liu2011321@sina.com>
2016-08-11 00:30:56 +0800

    websocket&springMVC

---
+++ src/main/java/per/liuqh/websocket/WebSocketHandshakeInterceptor.java
@@ -0,0 +1,21 @@
+package per.liuqh.websocket;
+
+import java.util.Map;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+public class WebSocketHandshakeInterceptor implements HandshakeInterceptor {
+
+	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+			Map<String, Object> attributes) throws Exception {
+		return true;
+	}
+
+	public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+			Exception exception) {
+
+	}
+}
