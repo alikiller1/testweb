249ea74d8e7d63b3f989abab8af376bf2f69139b
liuqinghua <liuqinghua@ppdai.com>
2016-08-25 15:57:22 +0800

    Signed-off-by: liuqinghua <liuqinghua@ppdai.com>

---
+++ src/main/java/per/liuqh/dao/IUserDao.java
@@ -0,0 +1,13 @@
+package per.liuqh.dao;
+
+/*import com.google.code.ssm.api.ParameterDataUpdateContent;
+import com.google.code.ssm.api.ParameterValueKeyProvider;
+
+import per.liuqh.entity.User;
+
+public interface IUserDao {
+	public void saveUser(User user);
+	public User getById(@ParameterValueKeyProvider String userId);
+	public void updateUser(@ParameterValueKeyProvider @ParameterDataUpdateContent User user);
+	public void deleteUser(@ParameterValueKeyProvider String userId);
+}*/
