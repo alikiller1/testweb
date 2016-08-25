1841adaa11ec1f76b67c120aefcb4412a85bbe3b
liuqinghua <liuqinghua@ppdai.com>
2016-08-25 17:57:49 +0800

    Signed-off-by: liuqinghua <liuqinghua@ppdai.com>

--- src/test/java/per/liuqh/dao/UserDaoTest.java
+++ src/test/java/per/liuqh/dao/UserDaoTest.java
@@ -2,6 +2,8 @@ package per.liuqh.dao;
 
 
 
+import net.spy.memcached.MemcachedClient;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,6 +14,8 @@ import per.liuqh.memcached.MemcachedService;
 public class UserDaoTest extends TestBaseCase{
 	@Autowired
 	private IUserDao iUserDao;
+	@Autowired
+	private MemcachedClient memcachedClient;
 	@Autowired
 	private MemcachedService memcachedService;
 	@Test
@@ -28,7 +32,13 @@ public class UserDaoTest extends TestBaseCase{
 	}
 	@Test
 	public void test2(){
-		System.out.println(iUserDao.getById("1"));
+		//System.out.println(iUserDao.getById("1"));
+		//memcachedClient.set("name", 0, "liuqh");
+		//System.out.println(memcachedService.getObject("ns:1"));
+		//System.out.println(System.currentTimeMillis());
+		//System.out.println(memcachedClient.get("name"));
+		System.out.println(memcachedClient.get("SERVER_PRIVATE_KEY"));
+		
 	}
 	
 	
