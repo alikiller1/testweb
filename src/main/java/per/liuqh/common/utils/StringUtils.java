f0fa9475dcb96b740667b3f8e4f3f95d01254749
liuqinghua <liuqinghua@paicaifu.com>
2016-11-16 15:49:12 +0800

    Signed-off-by: liuqinghua <liuqinghua@paicaifu.com>

--- src/main/java/per/liuqh/common/utils/StringUtils.java
+++ src/main/java/per/liuqh/common/utils/StringUtils.java
@@ -1,5 +1,7 @@
 package per.liuqh.common.utils;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class StringUtils {
 	public static boolean isNotBlank(String str){
 		if(null!=str&&!"".equals(str)){
@@ -12,5 +14,19 @@ public class StringUtils {
 	}
 	public static void main(String[] args) {
 		System.out.println(StringUtils.isBlank("abc"));
+	}
+	/**
+	 * 获得用户远程地址
+	 */
+	public static String getRemoteAddr(HttpServletRequest request) {
+		String remoteAddr = request.getHeader("X-Real-IP");
+		if (isNotBlank(remoteAddr)) {
+			remoteAddr = request.getHeader("X-Forwarded-For");
+		} else if (isNotBlank(remoteAddr)) {
+			remoteAddr = request.getHeader("Proxy-Client-IP");
+		} else if (isNotBlank(remoteAddr)) {
+			remoteAddr = request.getHeader("WL-Proxy-Client-IP");
+		}
+		return remoteAddr != null ? remoteAddr : request.getRemoteAddr();
 	}
 }
