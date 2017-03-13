0358bf3887d231aac201888036f68f537bdd6554
liuqinghua <liuqinghua@paicaifu.com>
2017-03-13 14:18:07 +0800

    Signed-off-by: liuqinghua <liuqinghua@paicaifu.com>

--- src/test/java/person/liuqh/utils/ThreadPoolTest.java
+++ src/test/java/person/liuqh/utils/ThreadPoolTest.java
@@ -5,12 +5,13 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 public class ThreadPoolTest {
 	public static void main(String[] args) {
 		ScheduledThreadPoolExecutor pool = new ScheduledThreadPoolExecutor(2);
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 20; i++) {
 			pool.submit(new MyThread());
 		}
-		System.out.println("task-" + pool.getTaskCount());
-		System.out.println("active-" + pool.getActiveCount());
+		//System.out.println("task-" + pool.getTaskCount());
+		//System.out.println("active-" + pool.getActiveCount());
 		pool.shutdown();
+		System.out.println("----------------");
 
 	}
 
