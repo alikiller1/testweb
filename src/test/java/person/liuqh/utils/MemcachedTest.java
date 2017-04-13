b1c7c5995afbffe434ff9182ce17643d0f9dc977
liuqinghua <liuqinghua@paicaifu.com>
2017-04-13 14:33:02 +0800

    Signed-off-by: liuqinghua <liuqinghua@paicaifu.com>

--- src/test/java/person/liuqh/utils/MemcachedTest.java
+++ src/test/java/person/liuqh/utils/MemcachedTest.java
@@ -11,6 +11,7 @@ import per.liuqh.entity.UserInfo;
 import per.liuqh.memcached.MemcachedUtils;
 
 public class MemcachedTest extends TestBaseCase {
+	private final static Object lock=new Object();
 	@Autowired
 	private IUserInfoDao userDao;
 	@Autowired
@@ -29,37 +30,92 @@ public class MemcachedTest extends TestBaseCase {
 		System.out.println(userDao.getById(user.getUserId()));
 
 	}
-	
+
 	@Test
 	public void test2() {
 		System.out.println(userDao.getById("2"));
-		 //System.out.println(userDao.getById("2"));
+		// System.out.println(userDao.getById("2"));
 		// memcachedClient.set("name", 0, "liuqh");
-		 System.out.println(memcachedUtils.getObject("userinfo:2"));
+		System.out.println(memcachedUtils.getObject("userinfo:2"));
 		// System.out.println(memcachedClient.get("name"));
-		//System.out.println(memcachedClient.get("SERVER_PRIVATE_KEY"));
+		// System.out.println(memcachedClient.get("SERVER_PRIVATE_KEY"));
 
 	}
+
 	@Test
-	public void test4(){
+	public void test4() {
 		UserInfo user = new UserInfo();
 		user.setUserId("1");
 		user.setPassword("234");
 		user.setUsername("liuqh");
 		userDao.updateUser(user);
 	}
+
 	@Test
-	public void test3(){
-		//memcachedClient.set("abc", 5, "abc_abc");
+	public void test3() {
+		// memcachedClient.set("abc", 5, "abc_abc");
 		System.out.println(memcachedClient.get("abc"));
 		System.out.println(userDao.getDateString("start"));
 		System.out.println(memcachedClient.get("key:start"));
-		System.out.println(System.currentTimeMillis()/1000);
+		System.out.println(System.currentTimeMillis() / 1000);
+	}
+
+	@Test
+	public void test6() {
+		System.out.println(userDao.getAssignStrings());
+	}
+
+	@Test
+	public void test7() throws InterruptedException {
+		memcachedUtils.put("test:abc", "0", 1000);
+		String result = memcachedUtils.get("test:abc");
+		System.out.println("init result=" + result);
+
+		for (int i = 0; i < 10000; i++) {
+			new Thread() {
+				public void run() {
+					String result = memcachedUtils.get("test:abc");
+					result = String.valueOf(Integer.parseInt(result) + 1);
+					memcachedUtils.put("test:abc", result, 1000);
+				};
+			}.start();
+		}
+	}
+
+	@Test
+	public void test8() {
+		System.out.println("end result=" + memcachedUtils.get("test:abc"));
 	}
 	
 	@Test
-	public void test6(){
-		System.out.println(userDao.getAssignStrings());
+	public void test9() throws InterruptedException {
+		new Thread(){public void run() {
+			synchronized (lock) {
+				System.out.println("start");
+				try {
+					lock.wait(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("end");
+			}
+		};}.start();
+		
+		Thread.sleep(1);
+		
+		/*Thread.sleep(5000);
+		System.out.println("dddddd");*/
+		
+		synchronized (lock) {
+			Thread.sleep(5000);
+			System.out.println("dddddd");
+		}
+		
+		
 	}
-	
+
 }
+
+
+
+   
