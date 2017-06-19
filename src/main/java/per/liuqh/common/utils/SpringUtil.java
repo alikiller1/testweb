2720790168c789fdab55d263d0d067d114c6e09d
liuqinghua <liuqinghua@LIUQINGHUA.pycredit.cn>
2017-06-19 18:45:37 +0800

    Signed-off-by: liuqinghua <liuqinghua@LIUQINGHUA.pycredit.cn>

--- src/main/java/per/liuqh/common/utils/SpringUtil.java
+++ src/main/java/per/liuqh/common/utils/SpringUtil.java
@@ -4,9 +4,13 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
-
+/**
+ * 用来从spring容器中获取bean
+ * @author :liuqinghua
+ * update time:2017年6月19日 下午6:44:46
+ */
 @Configuration
-public class SpringConfigTool implements ApplicationContextAware {
+public class SpringUtil implements ApplicationContextAware {
 
 	private static ApplicationContext context = null;
 
--- src/test/java/per/liuqh/utils/SpringConfigToolTest.java
+++ src/test/java/per/liuqh/utils/SpringConfigToolTest.java
@@ -3,14 +3,14 @@ package per.liuqh.utils;
 import org.junit.Test;
 
 import per.liuqh.common.TestBaseCase;
-import per.liuqh.common.utils.SpringConfigTool;
+import per.liuqh.common.utils.SpringUtil;
 import per.liuqh.service.UserService;
 
 
 public class SpringConfigToolTest extends TestBaseCase  {
 	@Test
 	public void testGetBean(){
-		UserService o=(UserService)SpringConfigTool.getBean("userServiceImpl");
+		UserService o=(UserService)SpringUtil.getBean("userServiceImpl");
 		System.out.println("========UserService="+o);
 	}
 }
